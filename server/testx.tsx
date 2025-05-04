e9edde085002d09d37a451b7a74e14cb621c7dee
“mythreyeepn <mythreyeepn@gmail.com>
2025-05-04 13:05:22 -0400

    h

--- server/testx.tsx
+++ server/testx.tsx
@@ -1,25 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TextField, Button, Stack } from '@mui/material'
 import { useBondStore } from '../store/bondStore'
-import { FormControl, InputLabel, Select, MenuItem } from '@mui/material'
 
-const TraderSelector = () => {
-  const { bonds, selectedTrader, setSelectedTrader } = useBondStore()
-  const traders = Array.from(new Set(bonds.map(b => b.trader)))
+const BulkUpdater = () => {
+  const [buySkew, setBuySkew] = useState('')
+  const [sellSkew, setSellSkew] = useState('')
+  const [dnt, setDnt] = useState('')
+  const { bulkUpdateVisible, bonds, selectedTrader } = useBondStore()
+
+  const handleBulkUpdate = () => {
+    const updates: any = {}
+    if (buySkew) updates.buySkew = parseFloat(buySkew)
+    if (sellSkew) updates.sellSkew = parseFloat(sellSkew)
+    if (dnt) updates.dnt = dnt
+
+    const filterFn = (bond: any) => bond.trader === selectedTrader
+    bulkUpdateVisible(filterFn, updates)
+  }
 
   return (
-    <FormControl fullWidth margin="normal">
-      <InputLabel>Trader</InputLabel>
-      <Select
-        value={selectedTrader}
-        onChange={e => setSelectedTrader(e.target.value)}
-        label="Trader"
-      >
-        {traders.map(trader => (
-          <MenuItem key={trader} value={trader}>{trader}</MenuItem>
-        ))}
-      </Select>
-    </FormControl>
+    <Stack direction="row" spacing={2} alignItems="center" mb={2}>
+      <TextField label="Buy Skew" value={buySkew} onChange={e => setBuySkew(e.target.value)} />
+      <TextField label="Sell Skew" value={sellSkew} onChange={e => setSellSkew(e.target.value)} />
+      <TextField label="DNT" value={dnt} onChange={e => setDnt(e.target.value)} />
+      <Button variant="contained" onClick={handleBulkUpdate}>Save</Button>
+    </Stack>
   )
 }
 
-export default TraderSelector
+export default BulkUpdater
